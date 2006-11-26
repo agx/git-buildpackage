9c3e7e62cd6d04b962c8375c1471eca3e540312b
Guido Guenther <agx@sigxcpu.org>
2006-11-26 14:55:12 +0100

    git-buildpackage: use pipes module instead of os.system and move some functions into the deb_utils module

--- git_buildpackage/deb_utils.py
+++ git_buildpackage/deb_utils.py
@@ -4,6 +4,7 @@
 
 import email
 import commands
+import os
 
 # When trying to parse a version-number from a dsc or changes file, these are
 # the valid characters.
@@ -21,4 +22,23 @@ def parse_changelog(changelog):
         cl['Debian-Version']=cl['Version']
     return cl
  
+
+def orig_file(cp):
+    "The name of the orig.tar.gz belonging to changelog cp"
+    return "%s_%s.orig.tar.gz" % (cp['Source'], cp['Upstream-Version'])
+
+
+def is_native(cp):
+    "Is this a debian native package"
+    return [ True, False ]['-' in cp['Version']]
+
+
+def has_orig(cp, dir):
+    "Check if orig.tar.gz exists in dir"
+    try:
+        os.stat("%s%s" % (dir,orig_file(cp)))
+    except OSError:
+        return False
+    return True
+
 # vim:et:ts=4:sw=4:
