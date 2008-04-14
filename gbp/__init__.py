df13ff980e9783c523f6b08c37e4b58e224876a3
Guido Guenther <agx@sigxcpu.org>
2008-04-14 14:14:09 +0200

    add vim modeline

--- gbp/__init__.py
+++ gbp/__init__.py
@@ -1,5 +1,6 @@
 # vim: set fileencoding=utf-8 :
 #
-# (C) 2006,2007 Guido Guenther <agx@sigxcpu.org>
+# (C) 2006,2007,2008 Guido Guenther <agx@sigxcpu.org>
 """The various things needed by git-buildpackage and friends"""
 
+# vim:et:ts=4:sw=4:et:sts=4:ai:set list listchars=tab\:»·,trail\:·:
