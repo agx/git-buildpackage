6cbbddf18e2a93c1518faec20ee3f18f64159b05
Guido Günther <agx@sigxcpu.org>
2009-08-23 13:34:38 +0200

    add unit test for --help

---
+++ tests/01_test_help.py
@@ -0,0 +1,15 @@
+# vim: set fileencoding=utf-8 :
+#
+# check if --help works
+
+import os
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def testhelp(self):
+        for prog in [ "buildpackage", "import-orig", "import-dsc", "dch" ]:
+            ret = os.system("./git-%s --help >/dev/null" % prog)
+            self.assertEqual(ret, 0)
+
+# vim:et:ts=4:sw=4:et:sts=4:ai:set list listchars=tab\:»·,trail\:·:
