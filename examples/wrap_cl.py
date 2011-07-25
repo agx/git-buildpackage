5bd3ff2361c68a3fdcd0b428a3e368cf05aae535
Guido Günther <agx@sigxcpu.org>
2011-07-25 23:02:29 +0200

    git-dch: add formatter that wraps the changelog entry
    Closes: #626439

---
+++ examples/wrap_cl.py
@@ -0,0 +1,14 @@
+# Simple changelog entry formatter
+#
+# It simply uses the built in formatter and linewraps the text
+#
+# Use git-dch --customizations=/usr/share/doc/git-buildpackage/examples/wrap_cl.py
+# or set it via gbp.conf
+
+import textwrap
+import gbp.dch
+
+def format_changelog_entry(commit_info, options, last_commit=False):
+    entry = gbp.dch.format_changelog_entry(commit_info, options, last_commit)
+    if entry:
+        return textwrap.wrap(" ".join(entry))
