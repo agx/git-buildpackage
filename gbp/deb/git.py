b05072150c7cdcadc8d797f33d10e3403996ff54
Guido Günther <agx@sigxcpu.org>
2012-01-21 17:37:13 +0100

    DebianGitRepository: fix module description

--- gbp/deb/git.py
+++ gbp/deb/git.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-"""A Debian Changelog"""
+"""A Git Repository that keeps a Debian Package"""
 
 import re
 from gbp.git import GitRepository, GitRepositoryError
