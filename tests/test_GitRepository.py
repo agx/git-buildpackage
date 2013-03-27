pombredanne/git-buildpackage-1

tests.test_GitRepository: import and setup gbp.log
In order to initialize gbp logging properly. Fixes false positives when
the GitRepository tests are run alone.

Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

tests/test_GitRepository.py
@@ -12,6 +12,10 @@ This testcase creates several repositores:
 """
 
 import os
+import gbp.log
+
+gbp.log.setup(color=True, verbose=True)
+
 repo_dir = os.path.abspath(
              os.path.join(os.path.curdir, 'gbp_%s_test_repo' % __name__))
 bare_dir = os.path.abspath(

agx/git-buildpackage

tests/test_GitRepository.py
@@ -12,6 +12,10 @@ This testcase creates several repositores:
 """
 
 import os
+import gbp.log
+
+gbp.log.setup(color=False, verbose=True)
+
 repo_dir = os.path.abspath(
              os.path.join(os.path.curdir, 'gbp_%s_test_repo' % __name__))
 bare_dir = os.path.abspath(