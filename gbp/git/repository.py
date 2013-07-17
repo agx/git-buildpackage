3c0a022c975369362498f6e5a0c9417735ef2e7e
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-07-17 19:13:42 +0300

    GitRepository.diff: prevent usage of external diff
    External diff might break patch generation, for example.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/git/repository.py
+++ gbp/git/repository.py
@@ -1538,7 +1538,7 @@ class GitRepository(object):
         @return: diff
         @rtype: C{str}
         """
-        options = GitArgs('-p')
+        options = GitArgs('-p', '--no-ext-diff')
         if stat is True:
             options.add('--stat')
         elif stat:
