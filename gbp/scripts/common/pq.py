pombredanne/git-buildpackage-1

pq.format_patch: support file path filtering
Implements a filter option that allows filtering out changes to certain
files/paths in the patch-generation. A commit is totally ignored if all
files would be filtered out. The path filter is given as a Python
regexp.

Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

gbp/scripts/common/pq.py
@@ -66,6 +66,22 @@ def pq_branch_base(pq_branch):
         return pq_branch[len(PQ_BRANCH_PREFIX):]
 
 
+def patch_path_filter(file_status, exclude_regex=None):
+    """
+    Create patch include paths, i.e. a "negation" of the exclude paths.
+    """
+    if exclude_regex:
+        include_paths = []
+        for fnlist in file_status.values():
+            for fn in fnlist:
+                if not re.match(exclude_regex, fn):
+                    include_paths.append(fn)
+    else:
+        include_paths = ['.']
+
+    return include_paths
+
+
 def write_patch_file(filename, repo, commit_info, diff):
     """Write patch file"""
     if not diff:
@@ -95,7 +111,8 @@ def write_patch_file(filename, repo, commit_info, diff):
     return filename
 
 
-def format_patch(outdir, repo, commit, patch_num, topic_regex=None):
+def format_patch(outdir, repo, commit, patch_num, topic_regex=None,
+                 path_exclude_regex=None):
     """Create patch of a single commit"""
     info = repo.get_commit_info(commit)
 
@@ -122,10 +139,16 @@ def format_patch(outdir, repo, commit, patch_num, topic_regex=None):
     filename = filename[:64-len(suffix)]
     filename = os.path.join(outdir, filename) + suffix
 
+    # Determine files to include
+    paths = patch_path_filter(info['files'], path_exclude_regex)
+
     # Finally, create the patch
-    diff = repo.diff('%s^!' % commit, stat=80, summary=True)
-    write_patch_file(filename, repo, info, diff)
-    return filename
+    if paths:
+        diff = repo.diff('%s^!' % commit, paths=paths, stat=80, summary=True)
+                         extra_opts=['--stat', '--summary', '-p'])
+        write_patch_file(filename, repo, info, diff)
+        return filename
+    return None
 
 
 def get_maintainer_from_control(repo):

agx/git-buildpackage

gbp/scripts/common/pq.py
@@ -66,6 +66,22 @@ def pq_branch_base(pq_branch):
         return pq_branch[len(PQ_BRANCH_PREFIX):]
 
 
+def patch_path_filter(file_status, exclude_regex=None):
+    """
+    Create patch include paths, i.e. a "negation" of the exclude paths.
+    """
+    if exclude_regex:
+        include_paths = []
+        for file_list in file_status.values():
+            for fname in file_list:
+                if not re.match(exclude_regex, fname):
+                    include_paths.append(fname)
+    else:
+        include_paths = ['.']
+
+    return include_paths
+
+
 def write_patch_file(filename, commit_info, diff):
     """Write patch file"""
     if not diff:
@@ -96,7 +112,7 @@ def write_patch_file(filename, commit_info, diff):
 
 
 def format_patch(outdir, repo, commit_info, series, numbered=True,
-                 topic_regex=None):
+                 topic_regex=None, path_exclude_regex=None):
     """Create patch of a single commit"""
     commit = commit_info['id']
 
@@ -130,11 +146,17 @@ def format_patch(outdir, repo, commit_info, series, numbered=True,
         filename = (num_prefix if numbered else '') + base + suffix
         filepath = os.path.join(outdir, filename)
 
+    # Determine files to include
+    paths = patch_path_filter(commit_info['files'], path_exclude_regex)
+
     # Finally, create the patch
-    diff = repo.diff('%s^!' % commit, stat=80, summary=True, text=True)
-    patch = write_patch_file(filepath, commit_info, diff)
-    if patch:
-        series.append(patch)
+    patch = None
+    if paths:
+        diff = repo.diff('%s^!' % commit, paths=paths, stat=80, summary=True,
+                         text=True)
+        patch = write_patch_file(filepath, commit_info, diff)
+        if patch:
+            series.append(patch)
     return patch
 
 