9455ae3934366f7dcd2586c284d413d6756856a8
Guido Günther <agx@sigxcpu.org>
2015-02-19 12:43:24 +0100

    Add string vs. binary encoding/decoding
    for Python3 compatibility

--- gbp/deb/format.py
+++ gbp/deb/format.py
@@ -82,9 +82,10 @@ class DebianSourceFormat(object):
         @returns: a debisn/source/format object
         @rtype: L{DebianSourceFormat}
 
+        >>> from six import b
         >>> import tempfile, os
         >>> with tempfile.NamedTemporaryFile(delete=False) as t:
-        ...    ret = t.write("3.0 (quilt)")
+        ...    ret = t.write(b("3.0 (quilt)"))
         >>> d = DebianSourceFormat.parse_file(t.name)
         >>> d.version
         '3.0'
