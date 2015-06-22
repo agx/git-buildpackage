b3341e5effe54b3f1bf9891857d537cb8501306b
Evgeni Golov <evgeni@debian.org>
2015-06-22 18:52:20 +0200

    Fix FSF address - thanks rpmlint
    use gnu.org/licences instead of a postal address, as suggested by Guido

--- gbp/__init__.py
+++ gbp/__init__.py
@@ -12,8 +12,8 @@
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#    along with this program; if not, please see
+#    <http://www.gnu.org/licenses/>
 """The various things needed by git-buildpackage and friends"""
 
 # vim:et:ts=4:sw=4:et:sts=4:ai:set list listchars=tab\:»·,trail\:·:
