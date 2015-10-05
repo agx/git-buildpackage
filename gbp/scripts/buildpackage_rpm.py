32d703fa26f74d2413f6e0ae22d994ac9f8c06df
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2015-10-05 14:49:31 +0300

    buildpackage_rpm: use wc_name from common
    Use the special "treeish" name for working copy as configured in
    gbp.common.buildpackage instead of hardcoding it in buildpackage_rpm.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/scripts/buildpackage_rpm.py
+++ gbp/scripts/buildpackage_rpm.py
@@ -36,7 +36,7 @@ from gbp.pkg import compressor_opts
 from gbp.rpm.git import GitRepositoryError, RpmGitRepository
 from gbp.rpm.policy import RpmPkgPolicy
 from gbp.tmpfile import init_tmpdir, del_tmpdir, tempfile
-from gbp.scripts.common.buildpackage import (index_name,
+from gbp.scripts.common.buildpackage import (index_name, wc_name,
                                              git_archive_submodules,
                                              git_archive_single, dump_tree,
                                              write_wc, drop_index)
@@ -165,7 +165,7 @@ def get_tree(repo, tree_name):
         if tree_name == index_name:
             # Write a tree of the index
             tree = repo.write_tree()
-        elif tree_name == 'WC':
+        elif tree_name == wc_name:
             # Write a tree of the working copy
             tree = write_wc(repo)
         else:
