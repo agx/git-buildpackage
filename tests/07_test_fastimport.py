6bc1b38edbb21f1e10bb105012b5c3c92753e4f0
Guido Günther <agx@sigxcpu.org>
2016-09-09 19:56:38 +0200

    07_test_fastimport: flake8 clean

--- tests/07_test_fastimport.py
+++ tests/07_test_fastimport.py
@@ -14,20 +14,24 @@ fastimport = None
 tf_name = 'testfile'
 tl_name = 'a_testlink'
 
+
 def setup():
     global repo
 
     tmpdir = context.new_tmpdir(__name__)
     repo = gbp.git.GitRepository.create(tmpdir.join('test_repo'))
 
+
 def teardown():
     context.teardown()
+
 
 def test_init_fastimport():
     """Create a fastimport object"""
     global fastimport
     fastimport = gbp.git.FastImport(repo)
     assert fastimport, "Failed to init FastImport"
+
 
 def test_add_file():
     """Add a file via fastimport"""
@@ -39,14 +43,17 @@ def test_add_file():
                         open(testfile),
                         os.path.getsize(testfile))
 
+
 def test_add_symlink():
     """Add a symbolic link via fastimport"""
     author = repo.get_author_info()
     fastimport.start_commit('master', author, "a 2nd commit")
     fastimport.add_symlink(tl_name, tf_name)
 
+
 def test_close():
     fastimport.close()
+
 
 def test_result():
     repo.force_head('master', hard=True)
@@ -57,4 +64,3 @@ def test_result():
     assert os.path.exists(testfile), "%s doesn't exist" % testfile
     assert os.path.lexists(testlink), "%s doesn't exist" % testlink
     assert os.readlink(testlink) == tf_name
-
