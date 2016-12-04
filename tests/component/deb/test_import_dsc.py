agx/git-buildpackage

commit 03276df5b9391dc877521cd6c3553ca071157991

Add ability to specify the clone target to gbp-import-{dsc,srpm}
Closes: #846567
Signed-off-by: Chris Lamb <lamby@debian.org>

gbp/scripts/import_dsc.py
@@ -216,7 +216,7 @@ def disable_pristine_tar(options, reason):
 def build_parser(name):
     try:
         parser = GbpOptionParserDebian(command=os.path.basename(name), prefix='',
-                                       usage='%prog [options] /path/to/package.dsc')
+                                       usage='%prog [options] /path/to/package.dsc [<target>]')
     except GbpError as err:
         gbp.log.err(err)
         return None
@@ -293,7 +293,7 @@ def main(argv):
         return ExitCodes.parse_error
 
     try:
-        if len(args) != 1:
+        if len(args) > 2:
             gbp.log.err("Need to give exactly one package to import. Try --help.")
             raise GbpError
         try:
@@ -324,12 +324,13 @@ def main(argv):
             print_dsc(src)
 
         if needs_repo:
-            if os.path.exists(src.pkg):
+            target = args[1] if len(args) >= 2 else src.pkg
+            if os.path.exists(target):
                 raise GbpError("Directory '%s' already exists. If you want to import into it, "
                                "please change into this directory otherwise move it away first."
-                               % src.pkg)
+                               % target)
             gbp.log.info("No git repository found, creating one.")
-            repo = DebianGitRepository.create(src.pkg)
+            repo = DebianGitRepository.create(target)
             os.chdir(repo.path)
 
         if repo.bare:
@@ -429,7 +430,7 @@ def main(argv):
             gbpc.RemoveTree(dirs[d])()
 
     if not ret and not skipped:
-        gbp.log.info("Version '%s' imported under '%s'" % (src.version, src.pkg))
+        gbp.log.info("Version '%s' imported under '%s'" % (src.version, repo.path))
     return ret
 
 

docs/manpages/gbp-import-dsc.sgml
@@ -1,3 +1,5 @@
+FIXME
+
 <refentry id="man.gbp.import.dsc">
   <refentryinfo>
     <address>
@@ -35,6 +37,7 @@
       <arg><option>--upstream-branch=</option><replaceable>branch_name</replaceable></arg>
       <arg><option>--upstream-tag=</option><replaceable>tag-format</replaceable></arg>
       <arg choice="plain"><replaceable>debian-source.dsc</replaceable></arg>
+      <arg choice="opt"><replaceable>target</replaceable></arg>
     </cmdsynopsis>
     <cmdsynopsis>
       &gbp-import-dsc;
@@ -58,7 +61,8 @@
     <para>
     If the command is run from within an existing repository, it will import
     into this; if not, a new repository named as the Debian source package is
-    created.
+    created. You can override the location of the new repository by specifying
+    the optional <replaceable>target</replaceable> argument.
     </para>
   </refsect1>
   <refsect1>

docs/manpages/gbp-import-srpm.sgml
@@ -1,3 +1,5 @@
+FIXME
+
 <refentry id="man.gbp.import.srpm">
   <refentryinfo>
     <address>
@@ -36,6 +38,7 @@
       <arg><option>--upstream-tag=</option><replaceable>TAG-FORMAT</replaceable></arg>
       <arg><option>--native</option></arg>
       <arg choice="plain"><replaceable>SRPM</replaceable></arg>
+      <arg choice="opt"><replaceable>target</replaceable></arg>
     </cmdsynopsis>
     <cmdsynopsis>
       &gbp-import-srpm;
@@ -54,7 +57,9 @@
     &gbp-import-srpm; imports an RPM source package into a &git; repository,
     notes the package version in the commit logs, and commits the change. All
     information, including package name, version and upstream source is
-    automatically detected from the source package. The tool supports importing
+    automatically detected from the source package but you can override the
+    location of the new repository by optionally specifying the
+    <replaceable>target</replaceable> argument. The tool supports importing
     both archived (src.rpm files) or unpacked (directory) source RPMs.
     </para>
   </refsect1>

gbp/scripts/import_srpm.py
@@ -122,7 +122,7 @@ def build_parser(name):
         parser = GbpOptionParserRpm(command=os.path.basename(name),
                                     prefix='',
                                     usage='%prog [options] /path/to/package'
-                                          '.src.rpm')
+                                          '.src.rpm [<target>]')
     except GbpError as err:
         gbp.log.err(err)
         return None
@@ -261,7 +261,8 @@ def main(argv):
         except GitRepositoryError:
             gbp.log.info("No git repository found, creating one.")
             is_empty = True
-            repo = RpmGitRepository.create(spec.name)
+            target = args[1] if len(args) == 2 else spec.name
+            repo = RpmGitRepository.create(target)
             os.chdir(repo.path)
 
         if repo.bare:
@@ -460,7 +461,7 @@ def main(argv):
         del_tmpdir()
 
     if not ret and not skipped:
-        gbp.log.info("Version '%s' imported under '%s'" % (ver_str, spec.name))
+        gbp.log.info("Version '%s' imported under '%s'" % (ver_str, repo.path))
     return ret
 
 

commit f631e64845e1d6584b94dd18da2b5e91d4c71d67

import_dsc: fail properly again without a package to import
(introduced by 03276df5b9391dc877521cd6c3553ca071157991)

gbp/scripts/import_dsc.py
@@ -216,7 +216,7 @@ def disable_pristine_tar(options, reason):
 def build_parser(name):
     try:
         parser = GbpOptionParserDebian(command=os.path.basename(name), prefix='',
-                                       usage='%prog [options] /path/to/package.dsc [<target>]')
+                                       usage='%prog [options] /path/to/package.dsc [target]')
     except GbpError as err:
         gbp.log.err(err)
         return None
@@ -293,7 +293,13 @@ def main(argv):
         return ExitCodes.parse_error
 
     try:
-        if len(args) > 2:
+        if len(args) == 1:
+            pkg = args[0]
+            target = None
+        elif len(args) == 2:
+            pkg = args[0]
+            target = args[1]
+        else:
             gbp.log.err("Need to give exactly one package to import. Try --help.")
             raise GbpError
         try:
@@ -309,7 +315,6 @@ def main(argv):
             needs_repo = True
             is_empty = True
 
-        pkg = args[0]
         if options.download:
             dsc = download_source(pkg,
                                   dirs=dirs,
@@ -324,7 +329,7 @@ def main(argv):
             print_dsc(src)
 
         if needs_repo:
-            target = args[1] if len(args) >= 2 else src.pkg
+            target = target or src.pkg
             if os.path.exists(target):
                 raise GbpError("Directory '%s' already exists. If you want to import into it, "
                                "please change into this directory otherwise move it away first."

tests/component/deb/test_import_dsc.py
@@ -160,3 +160,19 @@ class TestImportDsc(ComponentTestBase):
         self._check_repo_state(repo, 'master', ['master', 'upstream'],
                                tags=['upstream/2.6', 'debian/2.6-2'])
         assert len(repo.get_commits()) == 2
+
+    def test_target_dir(self):
+        """Test if setting the target dir works"""
+        def _dsc(version):
+            return os.path.join(DEB_TEST_DATA_DIR,
+                                'dsc-3.0',
+                                'hello-debhelper_%s.dsc' % version)
+        dsc = _dsc('2.6-2')
+        assert import_dsc(['arg0',
+                           '--verbose',
+                           '--no-pristine-tar',
+                           dsc,
+                           'targetdir']) == 0
+        assert os.path.exists('targetdir')
+        repo = ComponentTestGitRepository('targetdir')
+        self._check_repo_state(repo, 'master', ['master', 'upstream'])