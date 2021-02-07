11455c98c2d8f02acfd608af5c4ed701266e19ea
Andrej Shadura <andrew.shadura@collabora.co.uk>
2021-02-07 13:16:00 +0100

    gbp.git: Add support for long listing format (with object sizes)
    This is useful to be able to scan a tree for a specific file
    but only act if it’s non-empty.
    
    Signed-off-by: Andrej Shadura <andrew.shadura@collabora.co.uk>

--- tests/doctests/test_GitRepository.py
+++ tests/doctests/test_GitRepository.py
@@ -951,6 +951,8 @@ def test_make_tree():
     '745951810c9e22fcc6de9b23f05efd6ab5512123'
     >>> list(repo.list_tree(newtree, recurse=False, paths='testfile'))
     [('100644', 'blob', '19af7398c894bc5e86e17259317e4db519e9241f', b'testfile')]
+    >>> list(repo.list_tree(newtree, recurse=False, paths='testfile', sizes=True))
+    [('100644', 'blob', '19af7398c894bc5e86e17259317e4db519e9241f', 20, b'testfile')]
     >>> repo.make_tree([])
     '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
     """
