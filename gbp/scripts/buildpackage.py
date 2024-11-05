ea20cffc2bfc7bfa852c299d570182b30719e81c
Otto Kekäläinen <otto@debian.org>
2024-11-05 05:38:57 -0800

    Support numerical prefix in suite names
    E.g. '12-bookworm' or '24.04-noble'.
    
    Repositories that have many parallel releases in maintenance are easier
    to manage when the suites have a number prefix so they sort alphabetically
    correctly, and also causes less cognitive load in trying to remember what
    release number and name to put in debian/changelog.

--- gbp/scripts/buildpackage.py
+++ gbp/scripts/buildpackage.py
@@ -20,6 +20,7 @@
 import errno
 import os
 import pipes
+import re
 import shutil
 import shlex
 import sys
@@ -211,6 +212,9 @@ def get_pbuilder_dist(options, repo, native=False):
         parts = branch.rsplit('/')
         if len(parts) == 2:  # e.g. debian/stretch
             suite = parts[1]
+            # Strip numeral prefix if suite name has it
+            # e.g. '12-bookworm' or '24.04-noble'
+            suite = re.sub(r'^[0-9][0-9.]*-', '', suite)
             if vendor == parts[0]:
                 dist = '' if suite in ['sid', 'master', 'main', 'latest'] else suite
             else:
